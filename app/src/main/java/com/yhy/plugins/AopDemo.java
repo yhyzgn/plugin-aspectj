bf1964913c5904a6a0ae3c198116df4b95c8c4e2
yhyzgn <yhyzgn@gmail.com>
2019-04-05 16:51:11 +0800

    create

---
+++ app/src/main/java/com/yhy/plugins/AopDemo.java
@@ -0,0 +1,14 @@
+package com.yhy.plugins;
+
+/**
+ * author : 颜洪毅
+ * e-mail : yhyzgn@gmail.com
+ * time   : 2019-04-05 16:37
+ * version: 1.0.0
+ * desc   :
+ */
+public class AopDemo {
+
+    public void test() {
+    }
+}
